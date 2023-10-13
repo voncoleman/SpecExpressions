31964648cc0a403b1279af1039dc686f737060e7
Von Coleman <von.coleman@yahoo.com>
2023-10-12 22:27:59 -0500

    first commit

---
+++ src/SpecExpressions.java
@@ -0,0 +1,5 @@
+public class SpecExpressions { //You're really gonna comment yourself to death. Let Anubis know that you name your public class SpecExpressions, okay?
+    public static void main(String[] args){ //The beginning of the program. 
+        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / ( 40.5 - 4.5)); //My buddy, System.out.println displays the calculated data.
+    }
+}
